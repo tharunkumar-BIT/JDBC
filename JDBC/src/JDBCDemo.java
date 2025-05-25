6f345bad72882ded8d6676b6b32c03db9faacc90
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-25 22:44:40 +0530

    jdbc demo using sample table in mysql

--- JDBC/src/JDBCDemo.java
+++ JDBC/src/JDBCDemo.java
@@ -1,3 +1,22 @@
+import java.sql.*;
+
 public class JDBCDemo {
-    
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String userName = "root";
+        String password = "";
+        String query = "select * from employee";
+
+        Connection con = DriverManager.getConnection(url, userName, password);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+
+        // initially rs will be pointing to headings of the columns
+        rs.next(); // move to the first record
+        System.out.println("Id is " + rs.getInt(1));
+        System.out.println("Name is " + rs.getString(2));
+        System.out.println("Salary is " + rs.getInt(3));
+
+        con.close();
+    }
 }
