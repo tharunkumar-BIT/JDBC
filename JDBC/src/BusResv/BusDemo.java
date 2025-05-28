74a552bb198b44b78e84fa856791bbd59e8f659d
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-28 14:01:55 +0530

    exception handling

--- JDBC/src/BusResv/BusDemo.java
+++ JDBC/src/BusResv/BusDemo.java
@@ -4,30 +4,34 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class BusDemo {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 
 		BusDAO busdao = new BusDAO();
-		busdao.displayBusInfo();
+		try {
+			busdao.displayBusInfo();
 
-		int userOpt = 1;
-		Scanner scanner = new Scanner(System.in);
+			int userOpt = 1;
+			Scanner scanner = new Scanner(System.in);
 
-		while(userOpt == 1) {
-			System.out.println("Enter 1 to Book amd 2 to exit");
-			userOpt = scanner.nextInt();
-			if(userOpt == 1) {
-				Booking booking = new Booking();
-				if(booking.isAvailable()) {
-					BookingDAO bookingdao = new BookingDAO();
-					bookingdao.addBooking(booking);
-					System.out.println("Your booking is Confirmed");
-				}
-				else {
-					System.out.println("Sorry. Bus is full. Try another bus or date");
+			while (userOpt == 1) {
+				System.out.println("Enter 1 to Book amd 2 to exit");
+				userOpt = scanner.nextInt();
+				if (userOpt == 1) {
+					Booking booking = new Booking();
+					if (booking.isAvailable()) {
+						BookingDAO bookingdao = new BookingDAO();
+						bookingdao.addBooking(booking);
+						System.out.println("Your booking is Confirmed");
+					} else {
+						System.out.println("Sorry. Bus is full. Try another bus or date");
+					}
 				}
 			}
-		}
 
-		scanner.close();
+			scanner.close();
+		}
+		catch (Exception e){
+			System.out.println(e);
+		}
 	}
 }
